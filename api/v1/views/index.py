59064d18f8c26665eeab7e39799e0eba8295bed8
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-25 04:43:33 -0700

    Circular import solution

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 from flask import jsonify
-
+from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    from api.v1.views import app_views
     return jsonify({"status": "OK"})
