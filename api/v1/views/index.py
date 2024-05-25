62a9662c3c328c9a2551da5a715d7c028d1c450a
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-25 06:36:19 -0700

    chore: Add API endpoint for retrieving object statistics

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
 """Index module for the API"""
 from flask import jsonify
+from models import storage
 from api.v1.views import app_views
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+
+classes = {"amenities": Amenity, "cities": City, "places": Place,
+           "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats')
+def retrieve():
+    """Retrieves the number of each objects by type"""
+
+    items = {}
+    for key, value in classes.items():
+        items[key] = storage.count(value)
+
+    return jsonify(items)
