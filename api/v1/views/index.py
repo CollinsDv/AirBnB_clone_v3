8386cbad335f5ad93043f7847f94584adbb5e2cc
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-25 08:18:39 -0700

    chore: Update /stats API endpoint to allow or remove trailing slashes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -20,7 +20,7 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False)
 def retrieve():
     """Retrieves the number of each objects by type"""
     items = {}
