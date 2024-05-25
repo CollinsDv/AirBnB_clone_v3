7f2059412c47e2eadd1b0affe600614e7904a1a8
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-25 08:46:25 -0700

    chore: Update /stats API endpoint to allow or remove trailing slashes

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,8 @@
 """Main module for the API"""
 from flask import Flask
 from flask import Blueprint
+from flask import jsonify
+from flask import make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -15,8 +17,13 @@ app.register_blueprint(app_views)
 def close(err):
     storage.close()
 
+@app.errorhandler(404)
+def error_404(error):
+    return make_response(jsonify({"error": "Not found"}))
 
 if __name__ == "__main__":
     app.run(host=os.getenv("HBNB_API_HOST", "0.0.0.0"),
             port=int(os.getenv("HBNB_API_PORT", "5000")),
             threaded=True, debug=True)
+
+    
