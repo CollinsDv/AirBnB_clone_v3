890d2d2f762a2abdcdb9b1bc76977b7bc6ab0561
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-26 07:49:43 -0700

    feat: Add CRUD routes for places in API v1

---
+++ api/v1/views/places.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Cities module for the API"""
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+from models.city import City
+from models.place import Place
+
+@app_views.route('/api/v1/cities/<city_id>/places',
+                 methods=['GET'], strict_slashes=False)
+def places(city_id):
+    city = storage.get(City, city_id)
+    if place is None:
+        abort(404)
+    return jsonify([place.to_dict() for place in city.places])
+
+
+@app_views.route('/places/<place_id>',
+                 methods=['GET'], strict_slashes=False)
+def place_object(place_id):
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    return jsonify(place.to_dict())
+
+
+@app_views.route('/places/<place_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def place_delete(place_id):
+    place = storage.get(Place, city_id)
+    if place is None:
+        abort(404)
+    storage.delete(place)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/cities/<city_id>/places',
+                 methods=['POST'], strict_slashes=False)
+def place_create(city_id):
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    if 'name' not in data:
+        abort(400, description="Missing name")
+    place = Place(**data)
+    place.city_id = city_id
+    storage.new(place)
+    storage.save()
+    return jsonify(place.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>',
+                 methods=['PUT'], strict_slashes=False)
+def city_update(place_id):
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    for key, value in data.items():
+        if key not in ['id', 'user_id', 'created_at', 'updated_at']:
+            setattr(place, key, value)
+    storage.save()
+    return jsonify(place.to_dict()), 200
